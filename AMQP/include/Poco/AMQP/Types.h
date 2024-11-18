67942103f7dbd30f25b9055e2a02d98dfcd49fda
nyashbox <nyashbox@tutanota.com>
2024-11-18 23:25:48 +0200

    feat: add AMQP native integer types

---
+++ AMQP/include/Poco/AMQP/Types.h
@@ -0,0 +1,17 @@
+#ifndef AMQP_Types_INCLUDED
+#define AMQP_Types_INCLUDED
+
+#include <stdint.h>
+
+namespace Poco {
+namespace AMQP {
+
+using Octet = uint8_t;
+using Short = uint16_t;
+using Long = uint32_t;
+using LongLong = uint64_t;
+
+} // namespace AMQP
+} // namespace Poco
+
+#endif // AMQP_Types_INCLUDED
