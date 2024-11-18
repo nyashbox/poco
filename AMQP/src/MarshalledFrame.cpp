4d3aa35a20dabcc9091f8207f747d2673c47ba0f
nyashbox <nyashbox@tutanota.com>
2024-11-19 00:07:12 +0200

    refactor: use AMQP types instead of integers

--- AMQP/src/MarshalledFrame.cpp
+++ AMQP/src/MarshalledFrame.cpp
@@ -1,22 +1,24 @@
 #include "Poco/AMQP/Frames/MarshalledFrame.h"
+#include "Poco/AMQP/Types.h"
 
 #include <cstring>
-
-constexpr size_t TYPE_OFFSET = 0x00;
-constexpr size_t CHANNEL_OFFSET = 0x01;
-constexpr size_t SIZE_OFFSET = 0x03;
-
-constexpr size_t HEADER_SIZE = 0x07;
-constexpr uint8_t FRAME_END = 0xCE;
+#include <memory>
 
 namespace Poco {
 namespace AMQP {
 
-MarshalledFrame::MarshalledFrame(uint32_t size)
+constexpr AMQP::Octet TYPE_OFFSET = 0x00;
+constexpr AMQP::Octet CHANNEL_OFFSET = 0x01;
+constexpr AMQP::Octet SIZE_OFFSET = 0x03;
+
+constexpr AMQP::Octet HEADER_SIZE = 0x07;
+constexpr AMQP::Octet FRAME_END = 0xCE;
+
+MarshalledFrame::MarshalledFrame(AMQP::Long size)
     : MarshalledFrame(0x00, 0, size) {};
 
-MarshalledFrame::MarshalledFrame(uint8_t type, uint16_t channel,
-                                 uint32_t size) {
+MarshalledFrame::MarshalledFrame(AMQP::Octet type, AMQP::Short channel,
+                                 AMQP::Long size) {
   // Allocate frame memory
   _frameSize = size + HEADER_SIZE + sizeof(FRAME_END);
   _data = std::make_unique<std::byte[]>(_frameSize);
@@ -42,41 +44,42 @@ MarshalledFrame &MarshalledFrame::operator=(MarshalledFrame &&other) noexcept {
   return *this;
 };
 
-void MarshalledFrame::setChannel(const uint16_t channel) {
+void MarshalledFrame::setChannel(const AMQP::Short channel) {
   std::memcpy(&_data[CHANNEL_OFFSET], &channel, sizeof(channel));
 }
 
-void MarshalledFrame::setType(const uint8_t type) {
+void MarshalledFrame::setType(const AMQP::Octet type) {
   std::memcpy(&_data[TYPE_OFFSET], &type, sizeof(type));
 }
 
-void MarshalledFrame::setSize(const uint32_t size) {
+void MarshalledFrame::setSize(const AMQP::Long size) {
   std::memcpy(&_data[SIZE_OFFSET], &size, sizeof(size));
 }
 
-uint16_t MarshalledFrame::getChannel(void) const {
+AMQP::Short MarshalledFrame::getChannel(void) const {
   std::byte *channel = &_data[CHANNEL_OFFSET];
 
-  uint16_t ret = static_cast<uint8_t>(channel[0]) << 0 |
-                 static_cast<uint8_t>(channel[1]) << 8;
+  AMQP::Short ret = static_cast<AMQP::Octet>(channel[0]) << 0 |
+                    static_cast<AMQP::Octet>(channel[1]) << 8;
 
   return ret;
 }
 
-uint8_t MarshalledFrame::getType(void) const {
+AMQP::Octet MarshalledFrame::getType(void) const {
   std::byte *type = &_data[TYPE_OFFSET];
 
-  uint8_t ret = static_cast<uint8_t>(type[0]) << 0;
+  AMQP::Octet ret = static_cast<AMQP::Octet>(type[0]) << 0;
 
   return ret;
 }
 
-uint32_t MarshalledFrame::getSize(void) const {
+AMQP::Long MarshalledFrame::getSize(void) const {
   std::byte *size = &_data[SIZE_OFFSET];
 
-  uint32_t ret =
-      static_cast<uint8_t>(size[0]) << 0 | static_cast<uint8_t>(size[1]) << 8 |
-      static_cast<uint8_t>(size[2]) << 16 | static_cast<uint8_t>(size[3]) << 24;
+  AMQP::Long ret = static_cast<AMQP::Octet>(size[0]) << 0 |
+                   static_cast<AMQP::Octet>(size[1]) << 8 |
+                   static_cast<AMQP::Octet>(size[2]) << 16 |
+                   static_cast<AMQP::Octet>(size[3]) << 24;
 
   return ret;
 }
@@ -88,12 +91,12 @@ std::byte *MarshalledFrame::getPayload(void) const {
 }
 
 std::byte *MarshalledFrame::getData(void) const {
-  auto framePointer = _data.get();
+  std::byte *framePointer = _data.get();
 
   return framePointer;
 }
 
-size_t MarshalledFrame::getFrameSize(void) const { return _frameSize; }
+AMQP::Long MarshalledFrame::getFrameSize(void) const { return _frameSize; }
 
 } // namespace AMQP
 } // namespace Poco
