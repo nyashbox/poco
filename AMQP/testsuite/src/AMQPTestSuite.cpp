2ddd37fa4aacf282bffbc0fd34dd89f036046e60
nyashbox <nyashbox@tutanota.com>
2024-11-22 00:17:32 +0200

    test: add tests for Long/Short AMQP string types

--- AMQP/testsuite/src/AMQPTestSuite.cpp
+++ AMQP/testsuite/src/AMQPTestSuite.cpp
@@ -1,12 +1,14 @@
 #include "AMQPTestSuite.h"
 #include "AMQPTest.h"
 #include "MarshalledFrameTest.h"
+#include "TypesTest.h"
 
 CppUnit::Test *AMQPTestSuite::suite() {
   CppUnit::TestSuite *pSuite = new CppUnit::TestSuite("AMQPTestSuite");
 
   pSuite->addTest(AMQPTest::suite());
   pSuite->addTest(MarshalledFrameTest::suite());
+  pSuite->addTest(TypesTest::suite());
 
   return pSuite;
 }
