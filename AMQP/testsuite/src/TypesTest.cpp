57e589c70da49f57d3ccc2d0eb473623673222b8
nyashbox <nyashbox@tutanota.com>
2024-12-08 12:49:35 +0200

    style(TypesTest): update style according to style guide

--- AMQP/testsuite/src/TypesTest.cpp
+++ AMQP/testsuite/src/TypesTest.cpp
@@ -1,55 +1,77 @@
-#include "Poco/AMQP/Types.h"
-
 #include "CppUnit/TestCaller.h"
 #include "CppUnit/TestCase.h"
 #include "CppUnit/TestSuite.h"
+#include "Poco/AMQP/Types.h"
 #include "TypesTest.h"
+
 
 using Poco::AMQP::LongStr;
 using Poco::AMQP::Octet;
 using Poco::AMQP::ShortStr;
 
+
 const Octet longStringExpectedLength = 8;
 const Octet shortStringExpectedLength = 5;
 
-TypesTest::TypesTest(const std::string &name) : CppUnit::TestCase(name) {}
-TypesTest::~TypesTest() {};
 
-void TypesTest::testGetMarshalledSize() {
-  LongStr longString{"AAAA"};
-  ShortStr shortString{"AAAA"};
+TypesTest::TypesTest(const std::string &name) : 
+	CppUnit::TestCase(name) 
+{
+}
 
-  assertEquals(longString.getMarshalledSize(), longStringExpectedLength);
-  assertEquals(shortString.getMarshalledSize(), shortStringExpectedLength);
+
+TypesTest::~TypesTest() 
+{
+}
+
+
+void TypesTest::testGetMarshalledSize() 
+{
+	LongStr longString{"AAAA"};
+	ShortStr shortString{"AAAA"};
+
+	assertEquals(longString.getMarshalledSize(), longStringExpectedLength);
+	assertEquals(shortString.getMarshalledSize(), shortStringExpectedLength);
 };
 
-void TypesTest::testMarshalling() {
-  const Octet marshalledShortString[] = {0x04, 0x41, 0x41, 0x41, 0x41};
-  const Octet marshalledLongString[] = {0x00, 0x00, 0x00, 0x04,
-                                        0x41, 0x41, 0x41, 0x41};
 
-  LongStr longString{"AAAA"};
-  ShortStr shortString{"AAAA"};
+void TypesTest::testMarshalling() 
+{
+	const Octet marshalledShortString[] = {0x04, 0x41, 0x41, 0x41, 0x41};
+	const Octet marshalledLongString[] = {0x00, 0x00, 0x00, 0x04,
+										0x41, 0x41, 0x41, 0x41};
 
-  Octet buf[longStringExpectedLength];
+	LongStr longString{"AAAA"};
+	ShortStr shortString{"AAAA"};
 
-  longString.marshall(buf);
-  assertEquals(std::memcmp(marshalledLongString, buf, longStringExpectedLength),
-               0);
+	Octet buf[longStringExpectedLength];
 
-  shortString.marshall(buf);
-  assertEquals(
-      std::memcmp(marshalledShortString, buf, shortStringExpectedLength), 0);
+	longString.marshall(buf);
+	assertEquals(std::memcmp(marshalledLongString, buf, longStringExpectedLength),
+			   0);
+
+	shortString.marshall(buf);
+	assertEquals(
+	  std::memcmp(marshalledShortString, buf, shortStringExpectedLength), 0);
 };
 
-void TypesTest::setUp() {};
-void TypesTest::tearDown() {};
 
-CppUnit::Test *TypesTest::suite() {
-  CppUnit::TestSuite *pSuite = new CppUnit::TestSuite("TypesTest");
+void TypesTest::setUp() 
+{
+};
 
-  CppUnit_addTest(pSuite, TypesTest, testGetMarshalledSize);
-  CppUnit_addTest(pSuite, TypesTest, testMarshalling);
 
-  return pSuite;
+void TypesTest::tearDown() 
+{
+};
+
+
+CppUnit::Test *TypesTest::suite() 
+{
+	CppUnit::TestSuite *pSuite = new CppUnit::TestSuite("TypesTest");
+
+	CppUnit_addTest(pSuite, TypesTest, testGetMarshalledSize);
+	CppUnit_addTest(pSuite, TypesTest, testMarshalling);
+
+	return pSuite;
 };
--- AMQP/testsuite/src/TypesTest.h
+++ AMQP/testsuite/src/TypesTest.h
@@ -1,23 +1,27 @@
 #ifndef TypesTest_INCLUDED
 #define TypesTest_INCLUDED
 
+
 #include "CppUnit/TestCase.h"
 
-class TypesTest : public CppUnit::TestCase {
+
+class TypesTest : public CppUnit::TestCase 
+{
 public:
-  TypesTest(const std::string &name);
-  ~TypesTest();
+	TypesTest(const std::string &name);
+	~TypesTest();
 
-  void setUp();
-  void tearDown();
+	void setUp();
+	void tearDown();
 
-  void testGetMarshalledSize();
-  void testMarshalling();
+	void testGetMarshalledSize();
+	void testMarshalling();
 
-  static CppUnit::Test *suite();
+	static CppUnit::Test *suite();
 
 protected:
 private:
 };
+
 
 #endif // TypesTest_INCLUDED
