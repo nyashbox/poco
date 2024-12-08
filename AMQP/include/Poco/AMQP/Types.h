d8494e0dc99ad8eb5524280734dcc9d3d7ae3812
nyashbox <nyashbox@tutanota.com>
2024-12-09 00:49:45 +0200

    feat: implement FieldTable methods

--- AMQP/include/Poco/AMQP/Types.h
+++ AMQP/include/Poco/AMQP/Types.h
@@ -148,6 +148,77 @@ const AMQP::Octet *BaseStr<T>::cend()
 };
 
 
+inline FieldTable::FieldTable()
+{
+};
+
+
+inline FieldTable::~FieldTable()
+{
+};
+
+
+inline const Poco::Buffer<AMQP::Octet> &FieldTable::getBuffer() const 
+{
+	return _data;
+}
+
+
+template<>
+inline void FieldTable::insert(const AMQP::ShortStr &key, const AMQP::Octet value) 
+{
+	_insert(key, value, 'B');
+}
+
+
+template<>
+inline void FieldTable::insert(const AMQP::ShortStr &key, const AMQP::Short value)
+{
+	_insert(key, value, 'u');
+}
+
+
+template<>
+inline void FieldTable::insert(const AMQP::ShortStr &key, const AMQP::Long value)
+{
+	_insert(key, value, 'i');
+}
+
+
+template<>
+inline void FieldTable::insert(const AMQP::ShortStr &key, const AMQP::LongLong value)
+{
+	_insert(key, value, 'l');
+}
+
+
+template<>
+inline void FieldTable::insert(const AMQP::ShortStr &key, const AMQP::ShortStr value)
+{
+	_insert(key, value, 's');
+}
+
+
+template<>
+inline void FieldTable::insert(const AMQP::ShortStr &key, const AMQP::LongStr value)
+{
+	_insert(key, value, 'S');
+}
+
+
+template<typename T>
+inline void FieldTable::_insert(const AMQP::ShortStr &key, const T value, const AMQP::Octet type)
+{
+	_data.append(key.getBuffer());
+	_data.append(type);
+
+	if constexpr (std::is_unsigned_v<T>)
+		_data.append(reinterpret_cast<const AMQP::Octet *>(&value), sizeof(T));
+	else
+		_data.append(value.getBuffer());
+}
+
+
 } } // namespace Poco::AMQP
 
 
